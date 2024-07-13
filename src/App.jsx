bf21538d2228f5d0bc78064a700be8ea8beb6bcc
andradew <wilson00749@gmail.com>
2024-07-13 16:23:00 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Tres en raya</h1>
+  )
+}
+
+export default App
